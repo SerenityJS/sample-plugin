ac8ddce15c3409096656f8a85b8bb19dbebbe5b9
PMK744 <paytonmk744@outlook.com>
2024-04-13 15:04:16 -0500

    updated to latest version

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,11 @@
-import type { Logger, PlayerSpawned, Serenity } from '@serenityjs/serenity';
-import { BasePlugin } from '@serenityjs/serenity';
+import { BasePlugin } from "@serenityjs/plugins";
+import { ItemStack } from "@serenityjs/world";
+import { ItemType } from "@serenityjs/item";
+import { Packet } from "@serenityjs/protocol";
+import { BlockIdentifier, BlockPermutation } from "@serenityjs/block";
+
+import type { Serenity } from "launcher";
+import type { Logger } from "@serenityjs/logger";
 
 /**
  * Sample plugin for the Serenity server.
@@ -9,27 +15,46 @@ export default class SamplePlugin extends BasePlugin {
 	public constructor(serenity: Serenity, logger: Logger) {
 		super(serenity, logger);
 
-		// Listen to the PlayerSpawned event.
-		this.serenity.before('PlayerSpawned', this.onPlayerSpawned.bind(this));
-	}
+		// Log the plugin initialization.
+		logger.info("Sample plugin initialized.");
 
-	// Fires when the plugin is started.
-	public startup(): void {
-		this.logger.info('Sample plugin has started!');
-	}
+		// Since there are no events registered for Serenity yet, we will use the "before" method to listen for packets.
+		// This packet is sent when the local player is initialized. AKA the player spawned in the world.
+		serenity.network.before(Packet.SetLocalPlayerAsInitialized, (data) => {
+			// First we must get the player instance from the session.
+			// Which the Serenity instance provides.
+			const player = serenity.getPlayer(data.session);
 
-	// Fires when the plugin is stopped.
-	public shutdown(): void {
-		this.logger.info('Sample plugin has stopped!');
-	}
+			// Check if the player is null and return false if it is.
+			if (!player) return false; // This will stop the packet from being processed.
 
-	// Fires when a player sends a chat message.
-	protected onPlayerSpawned(event: PlayerSpawned): boolean {
-		// Send a welcome message to the player.
-		event.player.sendMessage('Welcome to the server!');
+			// Now lets add some items to the player's inventory.
+			// We will give the player glowing obsidian.
 
-		// Allow the event to continue.
-		// If false, the event will be cancelled.
-		return true;
+			// First we must find the permutation for the block.
+			const permutation = BlockPermutation.resolve(
+				BlockIdentifier.Glowingobsidian
+			);
+
+			// Now we can create a new item stack with the block.
+			const item = ItemStack.create(
+				ItemType.resolve(permutation.type), // Item type from the block type.
+				1, // Item amount.
+				permutation.index // The item metadata, which is eqivalent to the permutation index.
+			);
+
+			// Get the player's inventory component.
+			const inventory = player.getComponent("minecraft:inventory");
+
+			// Add the item to the inventory.
+			inventory.container.addItem(item);
+
+			// Now lets let the player know that we have added the item to their inventory.
+			player.sendMessage("Added glowing obsidian to your inventory!");
+
+			logger.info(
+				`Added glowing obsidian to the ${player.username}'s inventory.`
+			);
+		});
 	}
 }

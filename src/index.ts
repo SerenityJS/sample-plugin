bee6b61f1940167351efc7b520f3757b6a709de6
PMK744 <paytonmk744@outlook.com>
2024-06-03 20:14:01 -0500

    updated sample plugin

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,34 @@
-import { BasePlugin } from "@serenityjs/plugins";
-
 import type { Serenity } from "@serenityjs/launcher";
-import type { Logger } from "@serenityjs/logger";
+import type { Plugin } from "@serenityjs/plugins";
 
 /**
- * Sample plugin for the Serenity server.
+ * Fired when the plugin is registered
+ * @param data The data of the plugin, such as the logger and config
+ * @param serenity The serenity instance of the server
  */
-export default class SamplePlugin extends BasePlugin {
-	// The constructor is not required, as it is inherited from the BasePlugin class.
-	public constructor(serenity: Serenity, logger: Logger) {
-		super(serenity, logger);
-	}
+export function onRegister(data: Plugin): void {
+	// Get the logger of the plugin
+	const { logger } = data;
 
-	public startup(): void {
-		// Log the plugin startup.
-		this.logger.info("Sample plugin started.");
-	}
+	// Log that the plugin has been registered
+	logger.info("Plugin has been registered!");
+}
+
+/**
+ * Fired when the plugin is started
+ * @param serenity The serenity instance of the server
+ * @param data The data of the plugin, such as the logger and config
+ */
+export function onStartup(serenity: Serenity, data: Plugin): void {
+	// Get the logger of the plugin
+	const { logger } = data;
+
+	// Log that the plugin has been started
+	logger.info("Plugin has been started!");
+
+	// Wait for a player to spawn in the world
+	serenity.on("PlayerSpawned", (event) => {
+		// Send a message to the player
+		event.player.sendMessage("Hello, world!");
+	})
 }

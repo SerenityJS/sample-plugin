9c1da2859aed123c6fdcd6080f32fb09f042bbc8
PMK744 <paytonmk744@outlook.com>
2024-06-05 16:52:44 -0500

    updated readme

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# sample-plugin
+# Introduction
+The SerentityJS sample plugin provides a basic configuration for plugin usage in the software. To get started, either clone this repo or create a new repository using this template.
 
-Sample plugin provides a basic configuration for plugin usage in the SerenityJS software. As documentation has not yet been created, you will have to refrence the SerenityJS source code found on the main repo.
+## Usage
+The plugin system in SerenityJS is pretty simple to use and to develop on. Make sure you place your plugin within the `plugins` directory within your SerenityJS server. Once the server starts up, it will recognize the plugin and process it accordingly. Plugins that are written in TypeScript will be built upon usage. Visit our [documentation](https://serenityjs.net/) to get started!
--- src/index.ts
+++ src/index.ts
@@ -2,16 +2,19 @@ import type { Serenity } from "@serenityjs/launcher";
 import type { Plugin } from "@serenityjs/plugins";
 
 /**
- * Fired when the plugin is registered
- * @param data The data of the plugin, such as the logger and config
+ * Fired when the plugin is initialized
  * @param serenity The serenity instance of the server
+ * @param data The data of the plugin, such as the logger and config
  */
-export function onRegister(data: Plugin): void {
+export function onInitialize(serenity: Serenity, data: Plugin): void {
 	// Get the logger of the plugin
 	const { logger } = data;
 
 	// Log that the plugin has been registered
 	logger.info("Plugin has been registered!");
+
+	// This method should be used to register Blocks, Items, Entities, etc.
+	// Also should be used to register WorldProviders, TerrainGenerators, etc.
 }
 
 /**
@@ -31,4 +34,17 @@ export function onStartup(serenity: Serenity, data: Plugin): void {
 		// Send a message to the player
 		event.player.sendMessage("Hello, world!");
 	})
+}
+
+/**
+ * Fired when the plugin is stopped
+ * @param serenity The serenity instance of the server
+ * @param data The data of the plugin, such as the logger and config
+ */
+export function onShutdown(serenity: Serenity, data: Plugin): void {
+	// Get the logger of the plugin
+	const { logger } = data;
+
+	// Log that the plugin has been stopped
+	logger.info("Plugin has been stopped!");
 }
